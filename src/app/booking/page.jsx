2fad7753ef5750fc1c2c7c739f31f172524b03a6
Aarohi Bhanuj Chowdhary <146480441+bhanujongit4@users.noreply.github.com>
2024-08-07 11:06:29 +0530

    Update page.jsx

--- src/app/booking/page.jsx
+++ src/app/booking/page.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 import Footer from '../components/Footer';
+import FAQComponent from '../components/Faq';
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -20,7 +21,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const ContactForm = () => {
-
   const plans = [
     {
       title: "Basic Website",
@@ -57,6 +57,12 @@ const ContactForm = () => {
 
   const [submitted, setSubmitted] = useState(false);
 
+  const testimonials = [
+    { name: "John Doe", text: "Amazing service! My website looks fantastic." },
+    { name: "Jane Smith", text: "Professional and responsive team. Highly recommended!" },
+    { name: "Mike Johnson", text: "The e-commerce solution boosted my sales significantly." },
+  ];
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -79,80 +85,78 @@ const ContactForm = () => {
   return (
     <div className="bg-white dark:bg-black py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
-        <h2 className="text-3xl text-black dark:text-white text-center">Books Your Call</h2>
-        {submitted ? (
-          <p className="text-center text-green-500 mt-6">Thank you for your message! We will get back to you shortly.</p>
-        ) : (
-          <form onSubmit={handleSubmit} className="mt-8 max-w-lg mx-auto space-y-6">
-  <div>
-    <label className="block text-lg font-medium text-gray-700 dark:text-gray-300">Name</label>
-    <input
-      type="text"
-      name="name"
-      value={formData.name}
-      onChange={handleChange}
-      required
-      className="mt-1 block w-full md:w-full px-3 py-2 rounded-md border-gray-600 shadow-sm focus:border-yellow-500 focus:ring-yellow-500 bg-zinc-200 dark:bg-zinc-800 dark:border-gray-700 dark:text-white sm:text-sm"
-    />
-  </div>
+        <h2 className="text-3xl text-black dark:text-white text-center mb-12">Book Your Call</h2>
+        
+        <div className="flex flex-col md:flex-row gap-12">
+          {/* Contact Form */}
+          <div className="w-full md:w-1/2">
+            {submitted ? (
+              <p className="text-center text-green-500 mt-6">Thank you for your message! We will get back to you shortly.</p>
+            ) : (
+              <form onSubmit={handleSubmit} className="space-y-6">
+                <div>
+                  <label className="block text-lg font-medium text-zinc-bg-zinc-800 dark:text-gray-300">Name</label>
+                  <input
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    required
+                    className="mt-1 block w-full px-3 py-2 rounded-md border-gray-600 shadow-sm focus:border-yellow-500 focus:ring-yellow-500 bg-zinc-200 dark:bg-zinc-800 dark:border-zinc-bg-zinc-800 dark:text-white sm:text-sm"
+                  />
+                </div>
 
-  <div>
-    <label className="block text-lg font-medium text-gray-700 dark:text-gray-600">Email</label>
-    <input
-      type="email"
-      name="email"
-      value={formData.email}
-      onChange={handleChange}
-      required
-      className="mt-1 block w-full md:w-full px-3 py-2 rounded-md border-gray-300 shadow-sm focus:border-yellow-500 focus:ring-yellow-500 bg-zinc-200 dark:bg-zinc-800 dark:border-gray-700 dark:text-white sm:text-sm"
-    />
-  </div>
+                <div>
+                  <label className="block text-lg font-medium text-zinc-bg-zinc-800 dark:text-gray-300">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                    className="mt-1 block w-full px-3 py-2 rounded-md border-gray-300 shadow-sm focus:border-yellow-500 focus:ring-yellow-500 bg-zinc-200 dark:bg-zinc-800 dark:border-zinc-bg-zinc-800 dark:text-white sm:text-sm"
+                  />
+                </div>
 
-  <div>
-    <label className="block text-lg font-medium text-gray-700 dark:text-gray-300">Message</label>
-    <textarea
-      name="message"
-      value={formData.message}
-      onChange={handleChange}
-      required
-      className="mt-1 block w-full md:w-full px-3 py-2 rounded-md border-gray-300 shadow-sm focus:border-yellow-500 focus:ring-yellow-500 bg-zinc-200 dark:bg-zinc-800 dark:border-gray-700 dark:text-white sm:text-sm"
-      rows="4"
-    ></textarea>
-  </div>
+                <div>
+                  <label className="block text-lg font-medium text-zinc-bg-zinc-800 dark:text-gray-300">Message</label>
+                  <textarea
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    required
+                    className="mt-1 block w-full px-3 py-2 rounded-md border-gray-300 shadow-sm focus:border-yellow-500 focus:ring-yellow-500 bg-zinc-200 dark:bg-zinc-800 dark:border-zinc-bg-zinc-800 dark:text-white sm:text-sm"
+                    rows="4"
+                  ></textarea>
+                </div>
 
-  <button
-    type="submit"
-    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-lg font-medium rounded-md text-black bg-yellow-500 hover:bg-yellow-400 dark:text-white dark:bg-yellow-550 dark:hover:bg-yellow-400"
-  >
-    Submit
-  </button>
-</form>
+                <button
+                  type="submit"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-lg font-medium rounded-md text-black bg-yellow-500 hover:bg-yellow-400 dark:text-white dark:bg-yellow-550 dark:hover:bg-yellow-400"
+                >
+                  Submit
+                </button>
+              </form>
+            )}
+          </div>
 
-        )}
-      </div>
-
-      <div className="mt-16">
-        <h2 className="text-3xl font-extrabold text-black dark:text-white text-center">Our Plans</h2>
-        <div className="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto">
-          {plans.map((plan, index) => (
-            <div key={index} className="bg-gray-50 dark:bg-zinc-950 overflow-hidden shadow rounded-lg p-6">
-              <h3 className="text-xl font-bold text-gray-900 dark:text-white">{plan.title}</h3>
-              <p className="mt-4 text-gray-500 dark:text-gray-300">{plan.price}</p>
-              <ul className="mt-4 space-y-2 text-gray-600 dark:text-gray-400">
-                {plan.features.map((feature, i) => (
-                  <li key={i}>{feature}</li>
-                ))}
-              </ul>
-              <button
-                className="mt-6 w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-black bg-yellow-500 hover:bg-yellow-400 dark:text-white dark:bg-yellow-550 dark:hover:bg-yellow-400 md:py-4 md:text-lg md:px-10"
-              >
-                Choose Plan
-              </button>
+          {/* Testimonials */}
+          <div className="w-full md:w-1/2">
+            <h3 className="text-2xl text-black dark:text-white mb-6">What Our Clients Say</h3>
+            <div className="space-y-6">
+              {testimonials.map((testimonial, index) => (
+                <div key={index} className="bg-gray-100 dark:bg-zinc-800 p-4 rounded-lg">
+                  <p className="text-blue-600 dark:text-blue-600 italic">"{testimonial.text}"</p>
+                  <p className="text-black dark:text-white font-semibold mt-2">- {testimonial.name}</p>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
+
+       <FAQComponent></FAQComponent>
       </div>
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 };
