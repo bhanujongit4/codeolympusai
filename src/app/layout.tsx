67b748acde707752a052adf8b12860f845ba07af
Aarohi Bhanuj Chowdhary <146480441+bhanujongit4@users.noreply.github.com>
2024-08-07 06:41:23 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,17 @@ import { ReactNode } from 'react';
 interface RootLayoutProps {
   children: ReactNode;
 }
+export const metadata = {
+  title: "Aarohi Portfolio",
+  description: "This My Portfolio",
+};
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="images/logo-no-background.png" type="image/png" />
+      </head>
       <body className="min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-300">
         <ThemeProvider>
           <Header />
