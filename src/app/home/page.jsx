3b0d51a5565183f3477da581a6943ce15fedf80f
Aarohi Bhanuj Chowdhary <146480441+bhanujongit4@users.noreply.github.com>
2024-08-12 02:50:04 +0530

    Update page.jsx

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -56,7 +56,13 @@ const HomePage = () => {
 
           {/* Your Services Section */}
           <div className="mt-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <h2 className="text-3xl font-bold text-black dark:text-zinc-950 text-center sm:text-4xl">Services</h2>
+          <h2 className="text-3xl font-bold text-black  text-center sm:text-4xl flex items-center justify-center space-x-2">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-11">
+  <path d="M11.644 1.59a.75.75 0 0 1 .712 0l9.75 5.25a.75.75 0 0 1 0 1.32l-9.75 5.25a.75.75 0 0 1-.712 0l-9.75-5.25a.75.75 0 0 1 0-1.32l9.75-5.25Z" />
+  <path d="m3.265 10.602 7.668 4.129a2.25 2.25 0 0 0 2.134 0l7.668-4.13 1.37.739a.75.75 0 0 1 0 1.32l-9.75 5.25a.75.75 0 0 1-.71 0l-9.75-5.25a.75.75 0 0 1 0-1.32l1.37-.738Z" />
+  <path d="m10.933 19.231-7.668-4.13-1.37.739a.75.75 0 0 0 0 1.32l9.75 5.25c.221.12.489.12.71 0l9.75-5.25a.75.75 0 0 0 0-1.32l-1.37-.738-7.668 4.13a2.25 2.25 0 0 1-2.134-.001Z" />
+</svg>
+</h2>
             <div className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
               {[
                 { icon: <Layout className="w-6 h-6" />, title: 'Web Design', description: 'Creating beautiful, responsive websites' },
